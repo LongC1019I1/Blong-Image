b05d28a01c5c5b145c47ae6684a59e0f59ff0199
LongC1019I1 <bachthanhlong.it@gmail.com>
2020-03-19 04:53:14 +0700

    first commit

---
+++ resources/views/user/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    @include('user.layouts.head')
+    @include('user.layouts.header')
+</head>
+
+<body>
+
+<!-- Navigation -->
+
+@section('main-content')
+@show
+<!-- Footer -->
+@include('user/layouts/footer')
+
+</body>
+
+</html>
