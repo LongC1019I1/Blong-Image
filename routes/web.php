b095d11c9b2cc565f85cbf42d3af87dcde46b08d
BachLong <bachthanhlong.it@gmail.com>
2020-03-19 22:11:07 +0700

    blog admin

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('user/blog');
-});
+Route::get('/', 'User\HomeController@index');
 
-Route::get('post',function (){
-    return view('user/post');
-})->name('post');
+Route::get('post','User\PostController@index')->name('post');
+
+
+Route::resource('admin/post','Admin\PostController');
+
+Route::resource('admin/tag','Admin\TagController');
+
+Route::resource('admin/category','Admin\CategoryController');
